db8bf1d39ee0a41f41c58a0c754f157d6adfdf70
dstindl <denis.stindl@gmail.com>
2026-02-05 05:25:36 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,72 @@
+const CACHE_NAME = 'igelpflegestation-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install service worker
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Cache geöffnet');
+        return cache.addAll(urlsToCache).catch(err => {
+          console.log('Cache add error:', err);
+          return Promise.resolve();
+        });
+      })
+  );
+});
+
+// Fetch resources
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        
+        return fetch(event.request).then(
+          (response) => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+            
+            return response;
+          }
+        ).catch(() => {
+          // Return index.html for navigation requests when offline
+          if (event.request.mode === 'navigate') {
+            return caches.match('./index.html');
+          }
+          return new Response('Offline', { status: 503 });
+        });
+      }
+    )
+  );
+});
+
+// Activate service worker
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
